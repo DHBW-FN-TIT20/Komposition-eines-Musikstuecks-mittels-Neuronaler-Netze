8cfcc076c09146f92ce3e8e341cb207635b2a469
Glaser, Florian <Florian.Glaser@ifm.com>
2023-03-04 18:42:57 +0100

    train model with own data

--- mukkeBude/model/mukke_bude_transformer.py
+++ mukkeBude/model/mukke_bude_transformer.py
@@ -1,4 +1,5 @@
 import os
+
 import keras_nlp
 import tensorflow as tf
 from tensorflow import keras
@@ -21,11 +22,13 @@ EPOCHS = 6
 # Inference
 NUM_TOKENS_TO_GENERATE = 80
 
-class MukkeBudeTransformer():
+
+class MukkeBudeTransformer:
     def __init__(self, mapping):
         self.mapping = mapping
-        
+
         inputs = keras.layers.Input(shape=(None,), dtype=tf.int32)
+
         # Embedding.
         embedding_layer = keras_nlp.layers.TokenAndPositionEmbedding(
             vocabulary_size=len(mapping),
@@ -34,6 +37,7 @@ class MukkeBudeTransformer():
             mask_zero=True,
         )
         x = embedding_layer(inputs)
+
         # Transformer decoders.
         for _ in range(NUM_LAYERS):
             decoder_layer = keras_nlp.layers.TransformerDecoder(
@@ -41,85 +45,110 @@ class MukkeBudeTransformer():
                 intermediate_dim=FEED_FORWARD_DIM,
             )
             x = decoder_layer(x)  # Giving one argument only skips cross-attention.
+
         # Output.
         outputs = keras.layers.Dense(len(mapping))(x)
         self.model = keras.Model(inputs=inputs, outputs=outputs)
         loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
         perplexity = keras_nlp.metrics.Perplexity(from_logits=True, mask_token_id=0)
+
         self.model.compile(optimizer="adam", loss=loss_fn, metrics=[perplexity])
-        self.model.summary()
 
-    def train(self):
-        self.model.fit(self.train_ds, validation_data=self.val_ds, verbose=2, epochs=EPOCHS)
+    def train(
+        self,
+        path: os.PathLike,
+        vocabulary_size: int,
+        min_training_seq_len: int = 64,
+        batch_size: int = 64,
+        buffer_size: int = 256,
+    ) -> tf.keras.callbacks.History:
+        self.__loadDataset(path, vocabulary_size, min_training_seq_len, batch_size, buffer_size)
 
-    def generate(self, input):
+        self.train_ds = self.raw_train_ds.map(self.__preprocess, num_parallel_calls=tf.data.AUTOTUNE).prefetch(
+            tf.data.AUTOTUNE,
+        )
+        return self.model.fit(self.train_ds, verbose=2, epochs=EPOCHS)
+
+    def generate(self, input: str, max_length: int = 128) -> str:
         # Unpadded bos token.
-        prompt_tokens = tf.convert_to_tensor([self.tokenizer.token_to_id("[BOS]")])
-        output_tokens = keras_nlp.utils.greedy_search(
-        self.token_logits_fn,
-        prompt_tokens,
-        max_length=NUM_TOKENS_TO_GENERATE)
-        txt = self.tokenizer.detokenize(output_tokens)
-        print(f"Greedy search generated text: \n{txt}\n")
+        prompt_tokens = tf.convert_to_tensor([self.tokenizer.token_to_id(input)])
 
-    
+        print(f"prompt_tokens: {prompt_tokens}")
+
+        output_tokens = keras_nlp.utils.greedy_search(
+            self.token_logits_fn,
+            prompt_tokens,
+            max_length=max_length,
+        )
+        txt = self.tokenizer.detokenize(output_tokens)
+        clean_txt = self.__removeTokens(txt)
+        return clean_txt
+
     def token_logits_fn(self, inputs):
         cur_len = inputs.shape[1]
         output = self.model(inputs)
         return output[:, cur_len - 1, :]  # return next token logits
 
-
-    def loadDataset(self):
-        keras.utils.get_file(
-        origin="https://dldata-public.s3.us-east-2.amazonaws.com/simplebooks.zip",
-        extract=True,
-        )
-        dir = os.path.expanduser("~/.keras/datasets/simplebooks/")
-
-        # Load simplebooks-92 train set and filter out short lines.
+    def __loadDataset(
+        self,
+        path: os.PathLike,
+        vocabulary_size: int,
+        min_training_seq_len: int = 64,
+        batch_size: int = 64,
+        buffer_size: int = 256,
+    ):
+        # Load train set and filter out short lines.
         self.raw_train_ds = (
-            tf.data.TextLineDataset(dir + "simplebooks-92-raw/train.txt")
-            .filter(lambda x: tf.strings.length(x) > MIN_TRAINING_SEQ_LEN)
-            .batch(BATCH_SIZE)
-            .shuffle(buffer_size=256)
+            tf.data.TextLineDataset(path)
+            .filter(lambda x: tf.strings.length(x) > min_training_seq_len)
+            .batch(batch_size)
+            .shuffle(buffer_size=buffer_size)
         )
 
-        # Load simplebooks-92 validation set and filter out short lines.
-        self.raw_val_ds = (
-            tf.data.TextLineDataset(dir + "simplebooks-92-raw/valid.txt")
-            .filter(lambda x: tf.strings.length(x) > MIN_TRAINING_SEQ_LEN)
-            .batch(BATCH_SIZE)
-        )
-        
         # Train tokenizer vocabulary
+        vocabulary_size += 3  # Add 3 for [PAD], [UNK], [BOS].
         self.vocab = keras_nlp.tokenizers.compute_word_piece_vocabulary(
-        self.raw_train_ds,
-        vocabulary_size=len(self.mapping),
-        lowercase=True,
-        reserved_tokens=["[PAD]", "[UNK]", "[BOS]"])
-        
+            self.raw_train_ds,
+            vocabulary_size=vocabulary_size,
+            lowercase=True,
+            reserved_tokens=[
+                "[PAD]",  # padding token used to pad sequences to the same length /
+                "[UNK]",  # out-of-vocabulary (OOV) sub-words / unknown words are replaced with this token
+                "[BOS]",  # stands for beginning of sentence, but here technically it is a token representing the beginning of each line of training data
+            ],
+        )
+
+        # Load tokenizer
+        # WordPieceTokenizer is an efficient implementation of the WordPiece algorithm used by BERT and other models.
+        # It will strip, lower-case and do other irreversible preprocessing operations
         self.tokenizer = keras_nlp.tokenizers.WordPieceTokenizer(
-        vocabulary=self.vocab,
-        sequence_length=SEQ_LEN,
-        lowercase=True)
-        
+            vocabulary=self.vocab,
+            sequence_length=SEQ_LEN,
+            lowercase=True,
+        )
+
         # packer adds a start token
         self.start_packer = keras_nlp.layers.StartEndPacker(
             sequence_length=SEQ_LEN,
             start_value=self.tokenizer.token_to_id("[BOS]"),
         )
 
-    def preprocess(self, inputs):
+    def __preprocess(self, inputs):
         outputs = self.tokenizer(inputs)
         features = self.start_packer(outputs)
         labels = outputs
         return features, labels
 
-    def getDataset(self):
-        # Tokenize and split into train and label sequences.
-        self.train_ds = self.raw_train_ds.map(self.preprocess, num_parallel_calls=tf.data.AUTOTUNE).prefetch(
-            tf.data.AUTOTUNE
-        )
-        self.val_ds = self.raw_val_ds.map(self.preprocess, num_parallel_calls=tf.data.AUTOTUNE).prefetch(
-            tf.data.AUTOTUNE
-        )
+    def __removeTokens(self, inputs):
+        return self.__removeStartToken(self.__removePadding(inputs))
+
+    def __removePadding(self, inputs):
+        return tf.strings.regex_replace(inputs, "\s{0,2}\[PAD\]\s{0,2}", " ")
+
+    def __removeStartToken(self, inputs):
+        return tf.strings.regex_replace(inputs, "\s{0,2}\[BOS\]\s{0,2}", " ")
+
+    def __str__(self) -> str:
+        text = []
+        self.model.summary(print_fn=lambda x: text.append(x))
+        return "\n".join(text)
