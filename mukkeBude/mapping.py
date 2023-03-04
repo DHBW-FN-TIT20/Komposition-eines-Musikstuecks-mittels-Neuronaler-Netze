5dba1cdc3f778c16ef902e2c29bd1762eff88ba0
Glaser, Florian <Florian.Glaser@ifm.com>
2023-03-04 18:42:57 +0100

    train model with own data

--- mukkeBude/mapping.py
+++ mukkeBude/mapping.py
@@ -2,77 +2,91 @@ import pickle
 
 from utils import *
 
-BOS = 'xxbos'
-PAD = 'xxpad'
-EOS = 'xxeos'
+BOS = "xxbos"
+PAD = "xxpad"
+EOS = "xxeos"
 
-SEP = 'xxsep' # Used to denote end of timestep (required for polyphony). separator idx = -1 (part of notes)
+SEP = "xxsep"  # Used to denote end of timestep (required for polyphony). separator idx = -1 (part of notes)
 
-SPECIAL_TOKS = [BOS, PAD, EOS, SEP] # Important: SEP token must be last
+SPECIAL_TOKS = [BOS, PAD, EOS, SEP]  # Important: SEP token must be last
 
-NOTE_TOKS = [f'n{i}' for i in range(NOTE_SIZE)]
-DUR_TOKS = [f'd{i}' for i in range(DUR_SIZE)]
+NOTE_TOKS = [f"n{i}" for i in range(NOTE_SIZE)]
+DUR_TOKS = [f"d{i}" for i in range(DUR_SIZE)]
 NOTE_START, NOTE_END = NOTE_TOKS[0], NOTE_TOKS[-1]
 DUR_START, DUR_END = DUR_TOKS[0], DUR_TOKS[-1]
 
-class MusicMapping():
-    def __init__(self, itos:list[str]):
-        self.itos = itos
-        self.stoi = {v:k for k,v in enumerate(self.itos)}
 
-    def numericalize(self, tokens:list[str]) -> list[int]:
+class MusicMapping:
+    def __init__(self, itos: list[str]):
+        self.itos = itos
+        self.stoi = {v: k for k, v in enumerate(self.itos)}
+
+    def numericalize(self, tokens: list[str]) -> list[int]:
         "Convert a list of tokens `t` to their ids."
         return [self.stoi[w] for w in tokens]
 
-    def textify(self, nums:list[int], sep=' ') -> list[str]:
+    def textify(self, nums: list[int], sep=" ") -> list[str]:
         "Convert a list of `nums` to their tokens."
         items = [self.itos[i] for i in nums]
         return sep.join(items) if sep is not None else items
 
     @property
-    def pad_idx(self): return self.stoi[PAD]
+    def pad_idx(self):
+        return self.stoi[PAD]
+
     @property
-    def bos_idx(self): return self.stoi[BOS]
+    def bos_idx(self):
+        return self.stoi[BOS]
+
     @property
-    def sep_idx(self): return self.stoi[SEP]
+    def sep_idx(self):
+        return self.stoi[SEP]
+
     @property
-    def npenc_range(self): return (self.stoi[SEP], self.stoi[DUR_END]+1)
+    def npenc_range(self):
+        return (self.stoi[SEP], self.stoi[DUR_END] + 1)
+
     @property
-    def note_range(self): return self.stoi[NOTE_START], self.stoi[NOTE_END]+1
+    def note_range(self):
+        return self.stoi[NOTE_START], self.stoi[NOTE_END] + 1
+
     @property
-    def dur_range(self): return self.stoi[DUR_START], self.stoi[DUR_END]+1
+    def dur_range(self):
+        return self.stoi[DUR_START], self.stoi[DUR_END] + 1
 
     def is_duration(self, idx):
         return idx >= self.dur_range[0] and idx < self.dur_range[1]
+
     def is_duration_or_pad(self, idx):
         return idx == self.pad_idx or self.is_duration(idx)
 
     def __getstate__(self):
-        return {'itos':self.itos}
+        return {"itos": self.itos}
 
-    def __setstate__(self, state:dict):
-        self.itos = state['itos']
-        self.stoi = {v:k for k,v in enumerate(self.itos)}
+    def __setstate__(self, state: dict):
+        self.itos = state["itos"]
+        self.stoi = {v: k for k, v in enumerate(self.itos)}
 
-    def __len__(self): return len(self.itos)
+    def __len__(self):
+        return len(self.itos)
 
     def save(self, path):
         "Save `self.itos` in `path`"
-        pickle.dump(self.itos, open(path, 'wb'))
-        with open(path, 'w') as f:
+        pickle.dump(self.itos, open(path, "wb"))
+        with open(path, "w") as f:
             for line in self.itos:
                 f.write(f"{line} ")
 
     @classmethod
-    def create(cls) -> 'MusicMapping':
+    def create(cls) -> "MusicMapping":
         "Create a mapping from a set of `tokens`."
         itos = SPECIAL_TOKS + NOTE_TOKS + DUR_TOKS
-        if len(itos)%8 != 0:
-            itos = itos + [f'dummy{i}' for i in range(len(itos)%8)]
+        if len(itos) % 8 != 0:
+            itos = itos + [f"dummy{i}" for i in range(len(itos) % 8)]
         return cls(itos)
 
     @classmethod
     def load(cls, path):
         "Load the `Mapping` contained in `path`"
-        itos = pickle.load(open(path, 'rb'))
+        itos = pickle.load(open(path, "rb"))
         return cls(itos)
--- mukkeBude/utils.py
+++ mukkeBude/utils.py
@@ -1,24 +1,27 @@
+import json
+import os
+import textwrap
 from enum import Enum
 from itertools import groupby
-from tensorflow import keras
-import os
+from pathlib import Path
+from typing import List
+from typing import Union
+
 import music21 as m21
 import numpy as np
-from datasets import load_dataset
-import json
-from pathlib import Path
 
-BPB = 4 # beats per bar
-TIMESIG = f'{BPB}/4' # default time signature
+BPB = 4  # beats per bar
+TIMESIG = f"{BPB}/4"  # default time signature
 PIANO_RANGE = (21, 108)
-VALTSEP = -1 # separator value for numpy encoding
-VALTCONT = -2 # numpy value for TCONT - needed for compressing chord array
+VALTSEP = -1  # separator value for numpy encoding
+VALTCONT = -2  # numpy value for TCONT - needed for compressing chord array
 SAMPLE_FREQ = 4
 NOTE_SIZE = 128
-DUR_SIZE = (10*BPB*SAMPLE_FREQ)+1 # Max length - 8 bars. Or 16 beats/quarternotes
-MAX_NOTE_DUR = (8*BPB*SAMPLE_FREQ)
+DUR_SIZE = (10 * BPB * SAMPLE_FREQ) + 1  # Max length - 8 bars. Or 16 beats/quarternotes
+MAX_NOTE_DUR = 8 * BPB * SAMPLE_FREQ
 
-SEQType = Enum('SEQType', 'Mask, Sentence, Melody, Chords, Empty')
+SEQType = Enum("SEQType", "Mask, Sentence, Melody, Chords, Empty")
+
 
 def create_train_data_json(encoded_song: np.ndarray, sequence_length=32) -> list[dict]:
     num_sequences = len(encoded_song) - sequence_length
@@ -26,8 +29,8 @@ def create_train_data_json(encoded_song: np.ndarray, sequence_length=32) -> list
     data = []
 
     for i in range(num_sequences):
-        input = str(encoded_song[i:i+sequence_length])
-        target = str(encoded_song[i+sequence_length])
+        input = str(encoded_song[i : i + sequence_length])
+        target = str(encoded_song[i + sequence_length])
 
         data.append({"input": input, "labels": target})
 
@@ -38,11 +41,28 @@ def create_train_data_json(encoded_song: np.ndarray, sequence_length=32) -> list
 
     return data
 
-def create_train_data(encoded_song: str, path:str, sequence_length=32) -> None:
-    with open(path, 'a') as f:
-        f.append(f"{encoded_song}\n")
 
-def read_single(file_path:str) -> m21.stream.Score:
+def create_train_data(encoded_songs: List[str], path: os.PathLike, sequence_length=64) -> None:
+    """Create training data from encoded songs
+
+    Args:
+        encoded_songs (List[str]): list of encoded songs
+        path (os.PathLike): path to file
+        sequence_length (int, optional): length of sequence. Defaults to 64.
+    """
+    encoded_song = " ".join(encoded_songs)
+    splitted_song = textwrap.wrap(encoded_song, sequence_length)
+
+    # Check if file exists, if so, delete it
+    if os.path.exists(path):
+        os.remove(path)
+
+    with open(path, "a") as f:
+        for split in splitted_song:
+            f.write(split + "\n")
+
+
+def read_single(file_path: str) -> Union[m21.stream.Score, m21.stream.Part, m21.stream.Opus]:
     """Convert file of a song to music21.stream.Score. Accepted file types are .mid, .krn, .abc, .mxl, .musicxml
 
     Args:
@@ -53,25 +73,28 @@ def read_single(file_path:str) -> m21.stream.Score:
     """
     return m21.converter.parse(file_path)
 
-def write_midi(song:m21.stream.Score, output_path:str="test.mid") -> None:
+
+def write_midi(song: m21.stream.Score, output_path: str = "test.mid") -> None:
     """Export music21.stream.Score to midi format.
 
     Args:
         song (m21.stream.Score): the song
         output_path (str, optional): filename with path. Defaults to "test.mid".
     """
-    song.write('midi', fp=output_path)
-    
-def write_musicxml(song:m21.stream.Score, output_path:str="test.musicxml") -> None:
+    song.write("midi", fp=output_path)
+
+
+def write_musicxml(song: m21.stream.Score, output_path: str = "test.musicxml") -> None:
     """Export music21.stream.Score to musicxml format
 
     Args:
         song (m21.stream.Score): the song
         output_path (str, optional): filename with path. Defaults to "test.musicxml".
     """
-    song.write('musicxml', fp=output_path)
+    song.write("musicxml", fp=output_path)
 
-def read_single_from_corpus(corpus_path:str) -> m21.stream.Score:
+
+def read_single_from_corpus(corpus_path: str) -> m21.stream.Score:
     """Convert file in music21 corpus to music21.stream.Score. Paths to corpus files can be retrieved using e.g. paths = music21.corpus.getComposer('bach')
 
     Args:
@@ -82,7 +105,8 @@ def read_single_from_corpus(corpus_path:str) -> m21.stream.Score:
     """
     return m21.corpus.parse(corpus_path)
 
-def read_all(folder_path:str) -> list[m21.stream.Score]:
+
+def read_all(folder_path: str) -> List[Union[m21.stream.Score, m21.stream.Part, m21.stream.Opus]]:
     """Converts all files in folder to list[music21.stream.Score]. Accepted file types are .mid, .krn, .abc, .mxl, .musicxml
 
     Args:
@@ -98,26 +122,36 @@ def read_all(folder_path:str) -> list[m21.stream.Score]:
             songs.append(song)
     return songs
 
-def to_polyphonic_encoding(song:m21.stream.Score, mapping):
+
+def to_polyphonic_encoding(song: m21.stream.Score, mapping):
     score_arr = song_to_scorearr(song)
-    encoded_arr =  scorearr_to_encodedarr(score_arr)
+    encoded_arr = scorearr_to_encodedarr(score_arr)
     return encodedarr_to_indexencoding(encoded_arr, mapping)
 
-def from_polyphonic_encoding(index_arr, mapping, bpm:int=120) -> m21.stream.Score:
+
+def from_polyphonic_encoding(index_arr, mapping, bpm: int = 120) -> m21.stream.Score:
     encoded_arr = indexarr_to_encodedarr(index_arr, mapping)
     score_arr = encodedarr_to_scorearr(np.array(encoded_arr))
     return scorearr_to_song(score_arr, bpm=bpm)
 
-def song_to_scorearr(song:m21.stream.Score, note_size=NOTE_SIZE, sample_freq=SAMPLE_FREQ, max_note_dur=MAX_NOTE_DUR):
-    highest_time = max(song.flat.getElementsByClass('Note').highestTime, song.flat.getElementsByClass('Chord').highestTime)
-    maxTimeStep = round(highest_time * sample_freq)+1
+
+def song_to_scorearr(song: m21.stream.Score, note_size=NOTE_SIZE, sample_freq=SAMPLE_FREQ, max_note_dur=MAX_NOTE_DUR):
+    highest_time = max(
+        song.flat.getElementsByClass("Note").highestTime,
+        song.flat.getElementsByClass("Chord").highestTime,
+    )
+    maxTimeStep = round(highest_time * sample_freq) + 1
     score_arr = np.zeros((maxTimeStep, len(song.parts), NOTE_SIZE))
 
     def note_data(pitch, note):
-        return (pitch.midi, int(round(note.offset*sample_freq)), int(round(note.duration.quarterLength*sample_freq)))
+        return (
+            pitch.midi,
+            int(round(note.offset * sample_freq)),
+            int(round(note.duration.quarterLength * sample_freq)),
+        )
 
-    for idx,part in enumerate(song.parts):
-        notes=[]
+    for idx, part in enumerate(song.parts):
+        notes = []
         for elem in part.flat:
             if isinstance(elem, m21.note.Note):
                 notes.append(note_data(elem.pitch, elem))
@@ -128,12 +162,15 @@ def song_to_scorearr(song:m21.stream.Score, note_size=NOTE_SIZE, sample_freq=SAM
         # sort notes by offset (1), duration (2) so that hits are not overwritten and longer notes have priority
         notes_sorted = sorted(notes, key=lambda x: (x[1], x[2]))
         for n in notes_sorted:
-            if n is None: continue
-            pitch,offset,duration = n
-            if max_note_dur is not None and duration > max_note_dur: duration = max_note_dur
+            if n is None:
+                continue
+            pitch, offset, duration = n
+            if max_note_dur is not None and duration > max_note_dur:
+                duration = max_note_dur
             score_arr[offset, idx, pitch] = duration
-            score_arr[offset+1:offset+duration, idx, pitch] = VALTCONT      # Continue holding note
+            score_arr[offset + 1 : offset + duration, idx, pitch] = VALTCONT  # Continue holding note
     return score_arr
+
 
 def scorearr_to_encodedarr(chordarr, skip_last_rest=True):
     """Generate numpy array with [note,duration]
@@ -147,7 +184,7 @@ def scorearr_to_encodedarr(chordarr, skip_last_rest=True):
     """
     result = []
     wait_count = 0
-    for idx,timestep in enumerate(chordarr):
+    for idx, timestep in enumerate(chordarr):
         flat_time = encode_timestep(timestep)
         if len(flat_time) == 0:
             wait_count += 1
@@ -159,30 +196,32 @@ def scorearr_to_encodedarr(chordarr, skip_last_rest=True):
             wait_count = 1
     if wait_count > 0 and not skip_last_rest:
         result.append([VALTSEP, wait_count])
-    return np.array(result, dtype=int).reshape(-1, 2) # reshaping. Just in case result is empty
+    return np.array(result, dtype=int).reshape(-1, 2)  # reshaping. Just in case result is empty
+
 
 def encode_timestep(timestep, note_range=PIANO_RANGE, enc_type=None):
     # inst x pitch
     notes = []
-    for i,n in zip(*timestep.nonzero()):
-        d = timestep[i,n]
+    for i, n in zip(*timestep.nonzero()):
+        d = timestep[i, n]
         if d < 0:
-            continue # only supporting short duration encoding for now
+            continue  # only supporting short duration encoding for now
         if n < note_range[0] or n >= note_range[1]:
-            continue # must be within midi range
-        notes.append([n,d,i])
+            continue  # must be within midi range
+        notes.append([n, d, i])
 
-    notes = sorted(notes, key=lambda x: x[0], reverse=True) # sort by note (highest to lowest)
+    notes = sorted(notes, key=lambda x: x[0], reverse=True)  # sort by note (highest to lowest)
 
     if enc_type is None:
         # note, duration
         return [n[:2] for n in notes]
-    if enc_type == 'parts':
+    if enc_type == "parts":
         # note, duration, part
         return [n for n in notes]
-    if enc_type == 'full':
+    if enc_type == "full":
         # note_class, duration, octave, instrument
-        return [[n%12, d, n//12, i] for n,d,i in notes]
+        return [[n % 12, d, n // 12, i] for n, d, i in notes]
+
 
 def encodedarr_to_indexencoding(t, mapping, seq_type=SEQType.Sentence, add_eos=False):
     "Transforms numpy array from 2 column (note, duration) matrix to a single column"
@@ -198,6 +237,7 @@ def encodedarr_to_indexencoding(t, mapping, seq_type=SEQType.Sentence, add_eos=F
     suffix = np.array([mapping.stoi[EOS]]) if add_eos else np.empty(0, dtype=int)
     return np.concatenate([prefix, t.reshape(-1), suffix])
 
+
 def seq_prefix(seq_type, vocab):
     if seq_type == SEQType.Empty:
         return np.empty(0, dtype=int)
@@ -208,65 +248,72 @@ def seq_prefix(seq_type, vocab):
     #     start_token = vocab.stoi[MSEQ]
     return np.array([start_token, vocab.pad_idx])
 
+
 def encodedarr_to_scorearr(encoded_arr, note_size=NOTE_SIZE):
     num_instruments = 1 if len(encoded_arr.shape) <= 2 else encoded_arr.max(axis=0)[-1]
-    
+
     max_len = get_len_encodedarr(encoded_arr)
     # score_arr = (steps, inst, note)
     score_arr = np.zeros((max_len, num_instruments, note_size))
-    
+
     idx = 0
     for step in encoded_arr:
-        n,d,i = (step.tolist()+[0])[:3] # or n,d,i
-        if n < VALTSEP: continue # special token
+        n, d, i = (step.tolist() + [0])[:3]  # or n,d,i
+        if n < VALTSEP:
+            continue  # special token
         if n == VALTSEP:
             idx += d
             continue
-        score_arr[idx,i,n] = d
+        score_arr[idx, i, n] = d
     return score_arr
+
 
 def get_len_encodedarr(encoded_arr):
     duration = 0
     for t in encoded_arr:
-        if t[0] == VALTSEP: 
+        if t[0] == VALTSEP:
             duration += t[1]
     return duration + 1
 
+
 def scorearr_to_song(score_arr, sample_freq=SAMPLE_FREQ, bpm=120):
-    duration = m21.duration.Duration(1. / sample_freq)
+    duration = m21.duration.Duration(1.0 / sample_freq)
     stream = m21.stream.Score()
     stream.append(m21.meter.TimeSignature(TIMESIG))
     stream.append(m21.tempo.MetronomeMark(number=bpm))
     stream.append(m21.key.KeySignature(0))
     for inst in range(score_arr.shape[1]):
-        p = partarr_to_song(score_arr[:,inst,:], duration)
+        p = partarr_to_song(score_arr[:, inst, :], duration)
         stream.append(p)
     stream = stream.transpose(0)
     return stream
 
+
 def partarr_to_song(part_arr, duration):
     "convert instrument part to music21 chords"
     part = m21.stream.Part()
-    part.append(m21.instrument.Piano()) #TODO hier kann man das Instrument mitgeben
-    part_append_duration_notes(part_arr, duration, part) # notes already have duration calculated
+    part.append(m21.instrument.Piano())  # TODO hier kann man das Instrument mitgeben
+    part_append_duration_notes(part_arr, duration, part)  # notes already have duration calculated
     return part
+
 
 def part_append_duration_notes(part_arr, duration, stream):
     "convert instrument part to music21 chords"
-    for tidx,t in enumerate(part_arr):
-        note_idxs = np.where(t > 0)[0] # filter out any negative values (continuous mode)
-        if len(note_idxs) == 0: continue
+    for tidx, t in enumerate(part_arr):
+        note_idxs = np.where(t > 0)[0]  # filter out any negative values (continuous mode)
+        if len(note_idxs) == 0:
+            continue
         notes = []
         for nidx in note_idxs:
             note = m21.note.Note(nidx)
-            note.duration = m21.duration.Duration(part_arr[tidx,nidx]*duration.quarterLength)
+            note.duration = m21.duration.Duration(part_arr[tidx, nidx] * duration.quarterLength)
             notes.append(note)
         for g in group_notes_by_duration(notes):
             if len(g) == 1:
-                stream.insert(tidx*duration.quarterLength, g[0])
+                stream.insert(tidx * duration.quarterLength, g[0])
             else:
                 chord = m21.chord.Chord(g)
-                stream.insert(tidx*duration.quarterLength, chord)
+                stream.insert(tidx * duration.quarterLength, chord)
     return stream
 
 
@@ -275,28 +322,31 @@ def group_notes_by_duration(notes):
     "separate notes into chord groups"
     keyfunc = lambda n: n.duration.quarterLength
     notes = sorted(notes, key=keyfunc)
-    return [list(g) for k,g in groupby(notes, keyfunc)]
+    return [list(g) for k, g in groupby(notes, keyfunc)]
+
 
 def indexarr_to_encodedarr(index_arr, mapping, validate=True):
-    if validate: 
+    if validate:
         index_arr = validate_indexarr(index_arr, mapping.npenc_range)
     # convert from 1d arr two 2d arr
     index_arr = index_arr.copy().reshape(-1, 2)
     if index_arr.shape[0] == 0:
-        return index_arr      
+        return index_arr
     index_arr[:, 0] = index_arr[:, 0] - mapping.note_range[0]
     index_arr[:, 1] = index_arr[:, 1] - mapping.dur_range[0]
-    
+
     if validate:
         return validate_encodedarr(index_arr)
     return index_arr
 
+
 def validate_indexarr(t, valid_range):
     r = valid_range
     t = t[np.where((t >= r[0]) & (t < r[1]))]
-    if t.shape[-1] % 2 == 1: 
+    if t.shape[-1] % 2 == 1:
         t = t[..., :-1]
     return t
+
 
 def validate_encodedarr(t):
     is_note = (t[:, 0] < VALTSEP) | (t[:, 0] >= NOTE_SIZE)
@@ -304,8 +354,9 @@ def validate_encodedarr(t):
     invalid_dur_idx = (t[:, 1] < 0).argmax()
 
     invalid_idx = max(invalid_dur_idx, invalid_note_idx)
-    if invalid_idx > 0: 
-        if invalid_note_idx > 0 and invalid_dur_idx > 0: invalid_idx = min(invalid_dur_idx, invalid_note_idx)
-        print('Non midi note detected. Only returning valid portion. Index, seed', invalid_idx, t.shape)
+    if invalid_idx > 0:
+        if invalid_note_idx > 0 and invalid_dur_idx > 0:
+            invalid_idx = min(invalid_dur_idx, invalid_note_idx)
+        print("Non midi note detected. Only returning valid portion. Index, seed", invalid_idx, t.shape)
         return t[:invalid_idx]
     return t
