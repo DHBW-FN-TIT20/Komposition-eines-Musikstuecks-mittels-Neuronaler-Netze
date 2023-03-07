6bc57673ffc289db758ec5263772696e5edad3b6
screetox <80164220+screetox@users.noreply.github.com>
2023-03-07 13:35:24 +0100

    added examples from AI

--- midi-in-html/script.js
+++ midi-in-html/script.js
@@ -1,14 +1,17 @@
 const switchVisualization = () => {
-    const MidiPlayer = document.getElementById('midi-player');
-    const pianoRoll = document.getElementById('piano-roll-visualizer');
-    const staff = document.getElementById('staff-visualizer');
-    if (pianoRoll.style.visibility != 'hidden') {
-        pianoRoll.style.visibility = 'hidden';
-        staff.style.visibility = 'visible';
-        MidiPlayer.setAttribute('visualizer', '#staff-visualizer');
+    const midiPlayer = document.getElementById('midi-player');
+    const midiPlayer1 = document.getElementById('midi-player1');
+    const midiPlayer2 = document.getElementById('midi-player2');
+    const visButton = document.getElementById('vis-button');
+    if (midiPlayer.getAttribute('visualizer') == '#piano-roll-visualizer') {
+        midiPlayer.setAttribute('visualizer', '#staff-visualizer');
+        midiPlayer1.setAttribute('visualizer', '#staff-visualizer1');
+        midiPlayer2.setAttribute('visualizer', '#staff-visualizer2');
+        visButton.innerHTML = 'Follow on: Sheet Music';
     } else {
-        staff.style.visibility = 'hidden';
-        pianoRoll.style.visibility = 'visible';
-        MidiPlayer.setAttribute('visualizer', '#piano-roll-visualizer');
+        midiPlayer.setAttribute('visualizer', '#piano-roll-visualizer');
+        midiPlayer1.setAttribute('visualizer', '#piano-roll-visualizer1');
+        midiPlayer2.setAttribute('visualizer', '#piano-roll-visualizer2');
+        visButton.innerHTML = 'Follow on: Piano Roll';
     }
 }
