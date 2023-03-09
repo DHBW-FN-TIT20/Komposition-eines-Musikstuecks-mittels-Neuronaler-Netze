73ac6fe8d1dfe84eb11e1ba9c6d8571f21f0d21d
screetox <80164220+screetox@users.noreply.github.com>
2023-03-09 14:50:25 +0100

    improved sheet music visuals

--- flask-webapp/static/script.js
+++ flask-webapp/static/script.js
@@ -8,7 +8,6 @@ if (urlParams.get('example') == true || urlParams.get('example') == 'true') {
 let abcString = '';
 let xmlhttp = new XMLHttpRequest();
 const abcFileLocation = document.getElementById('abc-file-location').getAttribute('abcfilelocation');
-console.log(abcFileLocation);
 xmlhttp.open('GET', abcFileLocation, false);
 xmlhttp.onreadystatechange = function(){
     if(xmlhttp.status == 200 && xmlhttp.readyState == 4){
@@ -17,12 +16,33 @@ xmlhttp.onreadystatechange = function(){
 };
 xmlhttp.send();
 
-abcString = abcString ? abcString : 'X:1\nK:D\nDD AA|BBA2|\n';
+abcString = abcString ? abcString : 'X:1\nT:Keine passende ABC-Datei gefunden';
 abcString = abcString.split(/\r?\n/) // Split input text into an array of lines
     .filter(line => line.trim() !== "") // Filter out lines that are empty or contain only whitespace
     .join("\n"); // Join line array into a string
 
-let visualOptions = {  };
+let visualOptions = {
+    format: {
+        titlefont: 'Inconsolata 24',
+        subtitlefont: 'Inconsolata',
+        composerfont: 'Inconsolata',
+        partsfont: 'Inconsolata',
+        tempofont: 'Inconsolata',
+        gchordfont: 'Inconsolata',
+        annotationfont: 'Inconsolata',
+        infofont: 'Inconsolata',
+        textfont: 'Inconsolata',
+        vocalfont: 'Inconsolata',
+        wordsfont: 'Inconsolata'
+    },
+    // responsive: 'resize',
+    staffwidth: 740,
+    wrap: {
+        minSpacing: 1.5,
+        maxSpacing: 2.5,
+        preferredMeasuresPerLine: 4,
+    }
+};
 window.ABCJS.renderAbc('abcjs', abcString, visualOptions);
 
 let toggleLinks = () => {
