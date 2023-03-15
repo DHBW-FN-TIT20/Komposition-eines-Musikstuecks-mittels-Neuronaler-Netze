1c7764315393fe3c39203c2ced3a0818bb63e9c6
screetox <80164220+screetox@users.noreply.github.com>
2023-03-15 14:55:31 +0100

    changed example routing

--- flask-webapp/app.py
+++ flask-webapp/app.py
@@ -6,4 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/<midi>')
 def midi_viewer(midi=None):
-    return render_template('index.html', midi=midi, settings=settings, exampleFiles=exampleFiles)
+    return render_template('index.html', midi=midi, placeholder='Zelda', midiExample=False, settings=settings, exampleFiles=exampleFiles)
+
+@app.route('/examples')
+@app.route('/examples/<midiExample>')
+def midi_viewer_example(midiExample=None):
+    return render_template('index.html', midi=None, placeholder='Zelda', midiExample=midiExample, settings=settings, exampleFiles=exampleFiles)
