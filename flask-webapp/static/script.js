03bafdefd54b8626f5f2e7ca16e1d9259afda658
screetox <80164220+screetox@users.noreply.github.com>
2023-03-23 12:12:02 +0100

    small improvements, making use of jQuery

--- flask-webapp/static/script.js
+++ flask-webapp/static/script.js
@@ -1,11 +1,11 @@
-const links = document.getElementById('links');
-const infoButton = document.getElementById('toggleInfo');
-const help = document.getElementById('help');
-const helpButton = document.getElementById('toggleHelp');
+const links = $('#links');
+const infoButton = $('#toggleInfo');
+const help = $('#help');
+const helpButton = $('#toggleHelp');
 
 let abcString = '';
 let xmlhttp = new XMLHttpRequest();
-const abcFileLocation = document.getElementById('abc-file-location').getAttribute('abcfilelocation');
+const abcFileLocation = $('#abc-file-location').attr('abcfilelocation');
 xmlhttp.open('GET', abcFileLocation, false);
 xmlhttp.onreadystatechange = function(){
     if(xmlhttp.status == 200 && xmlhttp.readyState == 4){
@@ -16,8 +16,8 @@ xmlhttp.send();
 
 abcString = abcString ? abcString : 'X:1\nT:Keine passende ABC-Datei gefunden';
 abcString = abcString.split(/\r?\n/) // Split input text into an array of lines
-    .filter(line => line.trim() !== "") // Filter out lines that are empty or contain only whitespace
-    .join("\n"); // Join line array into a string
+    .filter(line => line.trim() !== '') // Filter out lines that are empty or contain only whitespace
+    .join('\n'); // Join line array into a string
 
 let visualOptions = {
     format: {
@@ -41,29 +41,29 @@ let visualOptions = {
         preferredMeasuresPerLine: 4,
     }
 };
-window.ABCJS.renderAbc('abcjs', abcString, visualOptions);
+ABCJS.renderAbc('abcjs', abcString, visualOptions);
 
 let toggleInfo = () => {
-    if (links.getAttribute('isHidden') == 'True') {
-        links.setAttribute('isHidden', 'False');
-        infoButton.innerHTML = 'x';
-        help.setAttribute('isHidden', 'True');
-        helpButton.innerHTML = '?';
+    if (links.attr('isHidden') == 'True') {
+        links.attr('isHidden', 'False');
+        infoButton.html('x');
+        help.attr('isHidden', 'True');
+        helpButton.html('?');
     } else {
-        links.setAttribute('isHidden', 'True');
-        infoButton.innerHTML = 'i';
+        links.attr('isHidden', 'True');
+        infoButton.html('i');
     }
 }
 
 let toggleHelp = () => {
-    if (help.getAttribute('isHidden') == 'True') {
-        help.setAttribute('isHidden', 'False');
-        helpButton.innerHTML = 'x';
-        links.setAttribute('isHidden', 'True');
-        infoButton.innerHTML = 'i';
+    if (help.attr('isHidden') == 'True') {
+        help.attr('isHidden', 'False');
+        helpButton.html('x');
+        links.attr('isHidden', 'True');
+        infoButton.html('i');
     } else {
-        help.setAttribute('isHidden', 'True');
-        helpButton.innerHTML = '?';
+        help.attr('isHidden', 'True');
+        helpButton.html('?');
     }
 }
 
@@ -81,12 +81,12 @@ let generate_new_song = () => {
     generateButton.prop('disabled', true);
 
     let params = {
-        model: $("input[type='radio'][name='model']:checked").val(),
-        length: $("input[type='radio'][name='length']:checked").val(),
-        music: $("input[type='radio'][name='music']:checked").val(),
-        coding: $("input[type='radio'][name='coding']:checked").val(),
-        instrument: $("input[type='radio'][name='instrument']:checked").val(),
-        bpm: $("input[type='radio'][name='bpm']:checked").val()
+        model: $(`input[type='radio'][name='model']:checked`).val(),
+        length: $(`input[type='radio'][name='length']:checked`).val(),
+        music: $(`input[type='radio'][name='music']:checked`).val(),
+        coding: $(`input[type='radio'][name='coding']:checked`).val(),
+        instrument: $(`input[type='radio'][name='instrument']:checked`).val(),
+        bpm: $(`input[type='radio'][name='bpm']:checked`).val()
     };
 
     let url = '/';
@@ -97,13 +97,13 @@ let generate_new_song = () => {
         success: d => {
             data = d;
         },
-        dataType: "text"
+        dataType: 'text'
     }).done(() => {
         console.log(`Data from /generate: ${data}`);
         url += data;
     }).fail(() => {
         url += '';
-        // alert('Leider ist ein Fehler aufgetreten. Bitte versuchen Sie es erneut oder wenden Sie sich an den Webseiten-Administrator.');
+        alert('Leider ist ein Fehler aufgetreten. Bitte versuchen Sie es erneut oder wenden Sie sich an den Webseiten-Administrator.');
     }).always(() => {
         document.location.href = url;
     });
